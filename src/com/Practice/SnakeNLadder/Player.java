4f58d59e4712eb9b03124771757b6227582724ae
PrakashML <prakashkrishnan08@gmail.com>
2024-01-23 11:35:55 +0530

    DieVal_uc2

--- src/com/Practice/SnakeNLadder/Player.java
+++ src/com/Practice/SnakeNLadder/Player.java
@@ -3,6 +3,11 @@ import java.lang.Math;
 public class Player {
     int position = 0;
     Player(){
-
+    }
+    public int Roll_Die(){
+        int max = 6;
+        int min = 1;
+        int range = max - min + 1;
+        return (int) (Math.random()*range + min);
     }
 }
